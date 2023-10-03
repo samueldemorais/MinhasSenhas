7de2db96876619c766c470ee0acdafc2a151f149
Samuel de Morais <ssamueldemorais@gmail.com>
2023-10-03 08:54:01 -0300

    first commit

---
+++ app/src/main/java/samuel/morais/minhassenhas/MainActivity.kt
@@ -0,0 +1,11 @@
+package samuel.morais.minhassenhas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/samuel/morais/minhassenhas/Password.kt
@@ -0,0 +1,11 @@
+package samuel.morais.minhassenhas
+
+class Password {
+    private lateinit var size: Number
+    private lateinit var descricao: String
+    private var number: Boolean = false
+    private var especial: Boolean = false
+    private var maiusculo: Boolean = false
+
+
+}
